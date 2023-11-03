362532aa64af1b172633585a84eeda8f4dc9eb76
Ahmet <ahmetcanlyev@gmail.com>
2023-11-03 22:15:12 +0500

    intro

---
+++ js/tab.js
@@ -0,0 +1,32 @@
+let tabContainer = document.querySelectorAll(".tabContainer");
+tabContainer.forEach((item) => {
+  const tabBtn = item.querySelectorAll(".tabBtn");
+  const tabEvent = item.querySelectorAll(".tabEvent");
+  tabBtn.forEach((e) => {
+    onTabClick(tabBtn, tabEvent, e);
+  });
+  function onTabClick(tabBtns, tabItems, item) {
+    item.addEventListener("click", function (e) {
+      let currentBtn = item;
+      let tabId = currentBtn.getAttribute("data-tab");
+      let currentTab = document.querySelector(tabId);
+      if (currentBtn.classList.contains("active")) {
+        const faq = currentBtn.parentElement.querySelector(".tabEvent");
+        if (faq) {
+          faq.classList.remove("active");
+          currentBtn.classList.remove("active");
+        }
+      } else if (!currentBtn.classList.contains("active")) {
+        tabBtns.forEach(function (item) {
+          item.classList.remove("active");
+        });
+
+        tabItems.forEach(function (item) {
+          item.classList.remove("active");
+        });
+        currentBtn.classList.add("active");
+        currentTab.classList.add("active");
+      }
+    });
+  }
+});
