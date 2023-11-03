777d962778a11e8ad8be639bc3fd86cbbc4a61a9
Ahmet <ahmetcanlyev@gmail.com>
2023-11-03 20:51:34 +0500

    intro

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
 var menu = document.getElementById("menu");
 var menuBtn = document.getElementById("menuBtn");
 var body = document.body;
+if (menuBtn) {
+  const closeMenu = () => {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("active");
+  };
 
-const closeMenu = () => {
-  menu.classList.remove("active");
-  menuBtn.classList.remove("active");
-  body.classList.remove("active");
-};
-
-menuBtn.onclick = function () {
-  menu.classList.toggle("active");
-  menuBtn.classList.toggle("active");
-  body.classList.toggle("active");
-};
-window.onclick = function (event) {
-  if (event.target == menu) {
-    closeMenu();
-  }
-};
-
-const header = document.getElementById("header");
-if (header)
-  window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > 400) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
+  menuBtn.onclick = function () {
+    menu.classList.toggle("active");
+    menuBtn.classList.toggle("active");
+    body.classList.toggle("active");
+  };
+  window.onclick = function (event) {
+    if (event.target == menu) {
+      closeMenu();
     }
+  };
+
+  const header = document.getElementById("header");
+  if (header)
+    window.addEventListener("scroll", () => {
+      if (document.documentElement.scrollTop > 400) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    });
+}
+
+let popUpBtn = document.getElementById("popUpBtn");
+if (popUpBtn) {
+  let popUp = document.getElementById("popUp");
+  let main = document.getElementById("main");
+  let header = document.getElementById("header");
+  popUpBtn.addEventListener("click", () => {
+    popUpBtn.classList.toggle("active");
+    main.classList.toggle("spacePopUp");
+    header.classList.toggle("spacePopUp");
+    popUp.classList.toggle("active");
   });
-  
+}
+
 // const links = document.querySelectorAll(".links");
 // const sections = document.querySelectorAll(".anchor");
 // function changeLinkState() {
@@ -95,7 +109,6 @@ if (header)
 //     }
 //   });
 // }
-
 
 // video ----------------
 // const video = document.querySelector("#video");
